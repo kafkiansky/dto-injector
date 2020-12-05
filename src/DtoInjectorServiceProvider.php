b2f641280c5d43cc1e147b3de113181275a3da83
v.zanfir <v.zanfir@corp.mail.ru>
2020-12-05 18:09:14 +0300

    Tag provider as deferrable, bind object populator default implementation

--- src/DtoInjectorServiceProvider.php
+++ src/DtoInjectorServiceProvider.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace Kafkiansky\DtoInjector;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Kafkiansky\DtoInjector\Mapper\ObjectPopulator;
+use Kafkiansky\DtoInjector\Mapper\PropertyTraverseObjectPopulator;
 use Kafkiansky\DtoInjector\Resolver\DtoResolverQualifier;
 use Illuminate\Contracts\Foundation\Application;
 
-final class DtoInjectorServiceProvider extends ServiceProvider
+final class DtoInjectorServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public function register(): void
     {
+        $this->app->bind(ObjectPopulator::class, PropertyTraverseObjectPopulator::class);
+
         $this->app->resolving(Dto::class, static function (Dto $dto, Application $container): Dto {
             /** @var Request $request */
             $request = $container->make(Request::class);
